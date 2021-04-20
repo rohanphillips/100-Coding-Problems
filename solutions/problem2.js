ea77ba70cc31333d032f406f8478b44e3ec05e62
Rohan Phillips <rohan@solutions-insurance.com>
2021-04-20 08:33:26 -0500

    Problem 2

--- solutions/problem2.js
+++ solutions/problem2.js
@@ -1,1 +1,15 @@
+// https://leetcode.com/problems/add-two-numbers/
+
 console.log("Problem2")
+
+const l1 = [2,4,3];
+const l2 = [5, 6, 4];
+
+const addTwoNumbers = function(l1, l2) {
+  var i = Number(l1.reverse().join(""));
+  console.log("i:", i)
+  var j = Number(l2.reverse().join(""));
+  return i + j;
+};
+
+console.log("result: ", addTwoNumbers(l1, l2));
