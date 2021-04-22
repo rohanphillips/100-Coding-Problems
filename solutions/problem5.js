a24467399418850672b95b3e857986b7a79ddcee
Rohan Phillips <rohan@solutions-insurance.com>
2021-04-22 13:59:02 -0500

    Problem5

--- solutions/problem5.js
+++ solutions/problem5.js
@@ -1,43 +1,27 @@
 
-const s = "civil"
-
-const isEqual =  (a1, a2) => {
-  let isEqual = true;
-  for(let i = 0; i < a1.length; i++) {
-    if (a1[i] !== a2[i]){
-      isEqual = false;
-      break;
-    }
-  } 
-  return isEqual;
-}
+const s = "aaaaaa"
 
 var longestPalindrome = function(s) {
   const splitString = s.split('') 
-  let template = [];
   let result;
 
   if (splitString.length === 0){
     return 0;
   }
   result =splitString[0];
-  for (let i=0; i<splitString.length; i++) {    
-    template = [];
-    template.push(splitString[i])
-    for (let j=i+1; j<splitString.length; j++) {
-      template.push(splitString[j]);
-      console.log("slice:", splitString.slice(i, i + j));
-      const mirror = template.map(a => a = a).reverse();      
-      if (mirror.join("") === template.join("")){
-        if (template.join("").length > result.length){
-          result = template.join("");   
-          console.log("result changed: ", result);                 
-        }
+  console.log("stringLength:", splitString.length / 2);
+  for (let j=0; j < splitString.length / 2 + 2; j++) {
+    const window = j+1;
+    for (let i=0; i<=splitString.length - window; i++) {     
+      let myString = splitString.slice(i, i + window).join("");
+      let mirror = JSON.parse(JSON.stringify(splitString.slice(i, i + window))).reverse().join("");
+      if (myString === mirror){
+        result = myString;
+        console.log("i:", i, " j:", j, "result:", result);
         break;
-      } 
-    }
-    console.log("next i")
-  } 
+      }
+    } 
+  }  
   return result;
 };
 console.log("result:", longestPalindrome(s))
