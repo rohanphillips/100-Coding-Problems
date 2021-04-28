0b0546a76be1cb2e245a55821b055a7bcb152000
Rohan Phillips <rohan@solutions-insurance.com>
2021-04-28 16:59:19 -0500

    Problem10 iteration

--- solutions/problem10.js
+++ solutions/problem10.js
@@ -1,12 +1,47 @@
-const s = "aa";
-const p = "a.*";
+const s = "aaaaab";
+const p = ".*";
 
 var isMatch = function(s, p) {
-  console.log("isValidString:", isValidString(s))
-  console.log("isValidPattern:", isValidPattern(p))
   if(isValidPattern(p) == false || isValidString(s) == false) return false
-  
+  let willRepeat = false;
+  let patternPointer = 0;
+  let currentChar = "";
+  for(let i = 0; i < s.length; i++) {
+    console.log("i:", i, " willRepeat:", willRepeat, " patternPointer:", patternPointer);
+    console.log("isAnyChar:", isAnyChar(p, patternPointer), " isRepeating:", isRepeating(p, patternPointer));
+    if(willRepeat == false){
+      if(isAnyChar(p, patternPointer) == false && isRepeating(p, patternPointer) == false) {
+        if(s[i] !== p[patternPointer]){
+          return false;
+        } else {
+          patternPointer++;
+        }
+      } else if(isAnyChar(p, patternPointer) && isRepeating(p, patternPointer) == false) {
+        patternPointer++;
+      } else if(isRepeating(p, patternPointer)){
+        willRepeat = true;
+        currentChar = s[i];
+        patternPointer = patternPointer + 2;
+      }
+    } else {
+      if (s[i] !== currentChar){
+        if(patternPointer >= p.length || s[i] !== p[patternPointer])
+        willRepeat = false;
+        
+        console.log("cancel willRepeat");
+      }
+    }
+  }
+  return true;
 };
+
+const isAnyChar = function(p, pointer) {
+  return p[pointer] == "."
+}
+
+const isRepeating = function(p, pointer) {
+  return p[pointer + 1] == "*";
+}
 
 const isValidString = function(s) {
   return /^[a-z]+$/g.test(s) == true ? true : false;
