8707047590c75517507c3759c7d865498047d95d
Rohan Phillips <rohan@solutions-insurance.com>
2021-05-03 17:05:09 -0500

    Problem12 Complete

---
+++ solutions/problem12.js
@@ -0,0 +1,18 @@
+const n=50;
+
+var intToRoman = function(num) {
+  const table = [[1000, "M"], [ 900, "CM"], [ 500, "D"], [400, "CD"], [100, "C"], [90, "XC"], [50, "L"], [40, "XL"], [10, "X"], [9, "IX"], [5, "V"], [4, "IV"], [1, "I"]] 
+  const result = []
+  for(let i=0; i<table.length; i++) {
+    const key = table[i];
+    const divisor = key[0];
+    let int = Math.trunc(num/divisor)
+    num = (num-int*divisor) ;
+    for(let j=0; j<int; j++) {
+      result.push(key[1])
+    }
+  }
+  return result.join("");
+};
+
+console.log("result:", intToRoman(n))
