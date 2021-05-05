2d36e4ebf8cde0da3e877f8c3796968a268596f9
Rohan Phillips <rohan@solutions-insurance.com>
2021-05-05 16:55:36 -0500

    Second Problem15 solution

--- solutions/problem15.js
+++ solutions/problem15.js
@@ -1,28 +1,68 @@
 
-const nums = [-1,0,1,2,-1,-4];
+const nums = [-1,0,1,2,-1,-4,4];
 var threeSum = function(nums) {
   if(nums.length <= 2) return [] 
   let output = [];
   let newTriple;
-  for(let i = 0; i < nums.length; i++){
-    for(let j = i + 1; j < nums.length; j++){
-      for(let k = j + 1; k < nums.length; k++){
-        if(i !== j && i !== k && j !== k){
-          let newTriple = [];
-          if(nums[i] + nums[j] + nums[k] === 0){
-            newTriple.push(nums[i]);
-            newTriple.push(nums[j]);
-            newTriple.push(nums[k]);
-            if(exists(newTriple.sort(), output) === false){
-              output.push(newTriple);
-            }
-          }
+  const sorted = nums.sort(function(a,b){return a - b})
+  let i = 0;
+  let j = sorted.length - 1;
+  let k = 0;
+  console.log("sorted:", sorted)
+  while(j - i > 1){
+    console.log("i:", i, "j:", j)
+    const mySum = sorted[i] + sorted[j];
+    console.log("mySum:", mySum)
+    if(mySum >= 0){
+      k = findKUp(i,j,sorted, mySum);
+      if(k > 0){
+        console.log("StoreUp", sorted[k])
+        newTriple = createNewTriple(i, k, j, sorted).sort();
+        if (exists(newTriple, output) === false){
+          output.push(newTriple);
         }
       }
+    } else if(mySum < 0){
+      k = findKDown(i,j,sorted, mySum);
+      if(k < 0){
+        console.log("StoreDn")
+      }
     }
+    i++;
+    j--;
   }
   return output.sort();
 };
+
+const createNewTriple = function (i, k, j, sorted){
+  let triple = [];
+  triple.push(sorted[i]);
+  triple.push(sorted[j]);
+  triple.push(sorted[k]);
+  return triple;
+}
+
+const findKUp = (i, j, sorted, find) => {
+  for(let k=i + 1; k < j; k++){
+    if(sorted[k] === find){
+      return k;
+    } else if(sorted[k] > find) {
+      return -1;
+    }
+  }
+  return -1;
+}
+
+const findKDown = (i, j, sorted, find) => {
+  for(let k=j-1; k >= 0; k--){
+    if(sorted[k] === find){
+      return k;
+    } else if(sorted[k] < find) {
+      return -1;
+    }
+  }
+  return -1;
+}
 
 const exists = (newTriple, store) => {
   for(let i = 0; i < store.length; i++){
